0b424b7630e34d66e1773751d00f024f4b54db47
Theflashbiel <gabrielcts10@gmail.com>
2025-04-02 19:48:03 -0300

    Primeira Aula

---
+++ Projeto Semaforo/semaphore/src/main/java/com/utfpr_fpoo/semaphore_light/Light.java
@@ -0,0 +1,13 @@
+package com.utfpr_fpoo.semaphore_light;
+
+public interface Light {
+    
+    enum OnOff{ON,OFF};
+
+    void turnOn();
+    void turnOff();
+
+    boolean isOn();
+    boolean isOff();
+
+}
